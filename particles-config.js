470dd2628353d1c6e25e31c14693e79b6952defb
kannekantidheeraj <kcrao.g1259@gmail.com>
2025-06-03 20:56:20 +0530

    Add files via upload

---
+++ particles-config.js
@@ -0,0 +1,42 @@
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 80
+      },
+      "color": {
+        "value": "#ff4d4d"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.5
+      },
+      "size": {
+        "value": 3
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 3
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        }
+      }
+    },
+    "retina_detect": true
+  });
+  
+  
